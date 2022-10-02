6b7f79997d62d619b7fafcef37bebbc8f711685f
lanomade <ganne.hannah@gmail.com>
2022-10-02 20:15:00 +0200

    add states

--- src/App.js
+++ src/App.js
@@ -2,42 +2,109 @@ import './App.css';
 import Section from './components/Section'
 import TipBtn from './components/TipBtn';
 import Amount from './components/Amount'
-import { useState, useContext } from 'react'
+import Input from './components/Input'
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [bill, setBill] = useState(0);
-  const [percentage, setPercentage] = useState(null);
-  const [numPeople, setNumPeople] = useState(null)
+  const [bill, setBill] = useState(0)
+  const [percentage, setPercentage] = useState(0)
+  const [customPercentage, setCustomPercentage] = useState('Custom');
+  const [numPeople, setNumPeople] = useState(0)
+  const [disabled, setDisabled] = useState(true)
+
+  const percentArr = [
+    { percent: 5 },
+    { percent: 10 },
+    { percent: 15 },
+    { percent: 25 },
+    { percent: 50 }
+  ]
+  const [activeIndex, setActiveIndex] = useState(-1)
+
+  useEffect(() => {
+    if (bill !== 0 && percentage !== 0 && numPeople !== 0) {
+      setDisabled(false)
+    } else {
+      setDisabled(true)
+    }
+  }, [bill, percentage, numPeople])
+
+  useEffect(() => {
+    setActiveIndex(-1)
+  }, [customPercentage])
 
   return (
     <>
       <h1>SPLI<br/>TTER</h1>
       <div className="container">
         <Section>
+          <Input
+            title="Bill"
+            bill={bill}
+            setBill={setBill}
+            numPeople={numPeople} 
+          />
           <div data-panel>
-            <h3>Bill</h3>
-            <input data-input-bill type="number" placeholder={0} onChange={event => setBill(event.target.value)} value={bill} />
-          </div>
-          <div data-panel>
-            <h3>Select Tip %</h3>
+            <h3 data-panel-title>Select Tip %</h3>
             <div className="btn-container">
-              <TipBtn value={0.05} setPercentage={setPercentage} />
-              <TipBtn value={0.1} setPercentage={setPercentage} />
-              <TipBtn value={0.15} setPercentage={setPercentage} />
-              <TipBtn value={0.25} setPercentage={setPercentage} />
-              <TipBtn value={0.50} setPercentage={setPercentage} />
-              <TipBtn color="green" setPercentage={setPercentage} />
+              {percentArr.map((item, index) =>
+                <TipBtn
+                  key={`tip-btn-${index + 1}`}
+                  index={index}
+                  value={item.percent}
+                  setPercentage={setPercentage}
+                  setActiveIndex={setActiveIndex} 
+                  className={index === activeIndex ? 'tip-btn active' : 'tip-btn'}
+                  color={item.color}
+                />
+              )}
+              <input
+                className="custom"
+                type="number"
+                placeholder="Custom"
+                onChange={event => {
+                  setCustomPercentage(event.target.value)
+                  setPercentage(event.target.value)
+                }}
+                value={customPercentage}   
+              />
             </div>
           </div>
-          <div data-panel>
-            <h3>Number of People</h3>
-            <input data-input-people type="number" placeholder={0} onChange={event => setNumPeople(event.target.value)} value={numPeople} />
-          </div>
+          <Input
+            title="Number of People"
+            bill={bill}
+            numPeople={numPeople}
+            setNumPeople={setNumPeople} 
+          />
+          {/* <div data-panel>
+            <div data-panel-title>
+              <h3>Number of People</h3>
+              <small>Can't be zero</small>
+            </div>
+            <input
+              type="number"
+              className={error? 'error' : ''}
+              placeholder={0}
+              onChange={event => setNumPeople(event.target.value)}
+              value={numPeople} 
+            />
+          </div> */}
         </Section>
         <Section color="green">
-          <Amount title="Tip Amount" amount={bill*percentage} />
-          <Amount title="Total" amount={bill*(1+percentage)/numPeople} />
-          <button className="reset-btn">RESET</button>
+          <Amount title="Tip Amount" amount={bill*(percentage/100)/numPeople} />
+          <Amount title="Total" amount={bill*(1+(percentage/100))/numPeople} />
+          <button
+            className={disabled ? 'reset-btn disabled' : 'reset-btn'}
+            onClick={() => {
+              setBill(0)
+              setPercentage(0)
+              setNumPeople(0)
+              setCustomPercentage('Custom')
+              setActiveIndex(-1)
+            }}
+          >
+            RESET
+          </button>
         </Section>
       </div>
     </>
