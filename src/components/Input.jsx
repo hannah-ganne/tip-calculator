36691e2c691d35ca32f4a17fbb272679832fe8b6
lanomade <ganne.hannah@gmail.com>
2022-10-02 22:53:50 +0200

    add icons in the input fields

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,4 @@
-import { useState, useEffect } from 'react'
-
-export default function Input({ title, bill, setBill, numPeople, setNumPeople }) {
+export default function Input({ title, bill, setBill, numPeople, setNumPeople, icon }) {
 
     return (
         <div data-panel>
@@ -8,18 +6,22 @@ export default function Input({ title, bill, setBill, numPeople, setNumPeople })
                 <h3>{title}</h3>
                 <small>Can't be zero</small>
             </div>
-            <input
-                type="number"
-                placeholder={0}
-                onChange={event => {
-                    if (title === 'Bill') {
-                        setBill(event.target.value)
-                    } else {
-                        setNumPeople(event.target.value)
-                    }
-                }}
-                value={title === 'Bill' ? bill : numPeople} 
-            />
+            <div className="input-container">
+                {icon}
+                <input
+                    type="number"
+                    placeholder={0}
+                    onChange={event => {
+                        if (title === 'Bill') {
+                            setBill(event.target.value)
+                        } else {
+                            setNumPeople(event.target.value)
+                        }
+                    }}
+                    onFocus={(event) => event.target.value = ''}
+                    value={title === 'Bill' ? bill : numPeople} 
+                />
+            </div>    
         </div>
     )
 }
