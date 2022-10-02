1f0255b39daf9b1d7ff828df9df7feaea31ce060
lanomade <ganne.hannah@gmail.com>
2022-10-02 20:15:25 +0200

    fix the NaN and infinity error

--- src/components/Amount.jsx
+++ src/components/Amount.jsx
@@ -1,11 +1,12 @@
-export default function Amount({ title, amount}) {
+export default function Amount({ title, amount }) {
+
     return (
         <div data-panel className="flex">
             <div>
                 <h3>{title}</h3>
                 <span className="person">/ person</span>
             </div>
-            <span className="amount">{`$${amount.toFixed(2)}`}</span>
+            <span className="amount">{Number.isNaN(amount) || amount === Infinity ? `$0.00` : `$${amount.toFixed(2)}`}</span>
         </div>
     )
 }
